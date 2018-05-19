c3b91b3edc053d8b6d522efb378ff8a4023e9203
Sasha Pachev <sasha@asksasha.com>
2018-05-19 16:46:25 -0600

    SQL schema update - added race and race_results tables

--- usatf.sql
+++ usatf.sql
@@ -1,7 +1,7 @@
 drop table if exists `member`;
 create table `member`
 (
-	`id` int not null auto_increment primary key,
+	`id` int unsigned not null auto_increment primary key,
 	`usatf_no` bigint not null unique key,
 	`fname` varchar(50) not null,
 	`mname` varchar(50) not null,
@@ -11,3 +11,27 @@ create table `member`
 	`gender` enum('M','F') not null,
 	`bdate` date not null
 ) engine=MyISAM;
+
+drop table if exists `race`;
+create table `race`
+(
+	`id` int unsigned not null auto_increment primary key,
+ `name` varchar(255) not null,
+ `date` date not null,
+ `dist_cm` int unsigned not null,
+ key(name),
+ key(`date`)
+);
+
+drop table if exists `race_results`;
+create table `race_results`
+(
+	`race_id` int unsigned not null,
+	`member_id` int unsigned not null,
+	`time_ms` int unsigned not null,
+	`place_overall` int unsigned not null,
+ 	`place_gender` int unsigned not null,
+ 	`place_div` int unsigned not null,
+ 	`place_gender_usatf` int unsigned not null,
+ 	`place_div_usatf` int unsigned not null
+);
